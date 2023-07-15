ea44bc542814eada18333c23b3d73da7827ce0e2
María Sánchez <msanchezroman95@gmail.com>
2023-07-15 11:29:26 +0200

    delete unnecessary code

--- src/components/new_recipe/NewRecipeIngredients.js
+++ src/components/new_recipe/NewRecipeIngredients.js
@@ -1,12 +1,8 @@
 const NewRecipeIngredients = ({ eachIngr, ingredients, updateIngredients }) => {
   const handleClickDelete = (ev) => {
     ev.preventDefault();
-    console.log(ev.currentTarget.parentNode.id);
-    console.log(ingredients);
     const findIngredient = ingredients.find((ingr) => ingr === eachIngr);
-    console.log(findIngredient);
     const findIngrIndex = ingredients.findIndex((ingr) => ingr === findIngredient);
-    console.log(findIngrIndex);
     updateIngredients(findIngrIndex);
   };
   return (
--- src/components/new_recipe/NewRecipeSteps.js
+++ src/components/new_recipe/NewRecipeSteps.js
@@ -2,11 +2,8 @@ const NewRecipeSteps = ({ eachStep, steps, updateSteps }) => {
   const handleClickDelete = (ev) => {
     ev.preventDefault();
     console.log(ev.currentTarget.parentNode.id);
-    console.log(steps);
     const findStep = steps.find((step) => step === eachStep);
-    console.log(findStep);
     const findStepIndex = steps.findIndex((step) => step === findStep);
-    console.log(findStepIndex);
     updateSteps(findStepIndex);
   };
   return (
