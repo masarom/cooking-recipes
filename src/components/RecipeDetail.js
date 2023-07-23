edbc6805a47c7b99bc7f1c69b47d5ed9688b3148
María Sánchez <msanchezroman95@gmail.com>
2023-07-23 12:44:51 +0200

    add RecipeDetail styles

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,33 +1,38 @@
+import ButtonGoBack from './buttons/ButtonGoBack';
+import '../styles/layout/RecipeDetail.scss';
+
 const RecipeDetail = ({ findRecipe }) => {
   // ingredients map
   const ingredients = findRecipe.ingredients.rawList.map((eachIngredient, i) => (
-    <li key={i} className='ingredient__list--item'>
+    <li key={i} className='item'>
       {eachIngredient}
     </li>
   ));
   // elaboration steps map
   const steps = findRecipe.elaboration.steps.map((eachStep, i) => (
-    <p key={i} className='elaboration__steps-item'>
+    <li key={i} className='item'>
       {eachStep}
-    </p>
+    </li>
   ));
   if (findRecipe) {
     return (
-      <>
-        <h2>{findRecipe.title}</h2>
-        <img className='recipe__image' src={findRecipe.image} alt={`Foto de ${findRecipe.title}`} />
-        <section className='ingredients'>
-          <h3>Necesitamos</h3>
-          <ul className='ingredient--list'>{ingredients}</ul>
+      <section className='recipe'>
+        <h2 className='recipe__title'>{findRecipe.title}</h2>
+        <div className='recipe__image' style={{ backgroundImage: `url(${findRecipe.image})` }}></div>
+        <section className='recipe__initialComment'>{findRecipe.initialComment}</section>
+        <section className='recipe__ingredients' id='#ingredients'>
+          <h3 className='recipe__ingredients--title'>Necesitamos</h3>
+          <ul className='recipe__ingredients--list'>{ingredients}</ul>
         </section>
-        <section className='elaboration__steps'>
-          <h3>Elaboración</h3>
-          {steps}
+        <section className='recipe__elaboration' id='#elaboration'>
+          <h3 className='recipe__elaboration--title'>Elaboración</h3>
+          <ul className='recipe__elaboration--list'>{steps}</ul>
         </section>
         <section>
           <p>{findRecipe.elaboration.finalQuote}</p>
         </section>
-      </>
+        <ButtonGoBack />
+      </section>
     );
   }
 };
